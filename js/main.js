b67bdff9ee0bbf1b4249be453c5b76dda74f8f00
Sergio Rocha <rochascript@gmail.com>
2019-01-15 11:34:16 -0600

    Added a features section

--- js/main.js
+++ js/main.js
@@ -45,11 +45,11 @@
           if (settings.sticky === true) cssmenu.css('position', 'fixed');
   
           resizeFix = function() {
-            if ($( window ).width() > 768) {
+            if ($( window ).width() > 752) {
               cssmenu.find('ul').show();
             }
   
-            if ($(window).width() <= 768) {
+            if ($(window).width() <= 752) {
               cssmenu.find('ul').hide().removeClass('open');
             }
           };
