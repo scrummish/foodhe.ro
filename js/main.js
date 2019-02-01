7a79d1f44ba0402df34b667a525b5607db076e35
Sergio Rocha <sergio@Sergios-MacBook-Pro.local>
2019-02-01 00:18:26 -0600

    Added a donation section and a material design style button

--- js/main.js
+++ js/main.js
@@ -68,6 +68,44 @@
         });
     };
   })(jQuery);
+
+  (function($) {
+    $.fn.rkmd_rippleEffect = function() {
+      var btn, self, ripple, size, rippleX, rippleY, eWidth, eHeight;
+  
+      btn = $(this).not('[disabled], .disabled');
+  
+      btn.on('mousedown', function(e) {
+        self = $(this);
+  
+        // Disable right click
+        if(e.button === 2) {
+          return false;
+        }
+  
+        if(self.find('.ripple').length === 0) {
+          self.prepend('<span class="ripple"></span>');
+        }
+        ripple = self.find('.ripple');
+        ripple.removeClass('animated');
+  
+        eWidth = self.outerWidth();
+        eHeight = self.outerHeight();
+        size = Math.max(eWidth, eHeight);
+        ripple.css({'width': size, 'height': size});
+  
+        rippleX = parseInt(e.pageX - self.offset().left) - (size / 2);
+        rippleY = parseInt(e.pageY - self.offset().top) - (size / 2);
+  
+        ripple.css({ 'top': rippleY +'px', 'left': rippleX +'px' }).addClass('animated');
+  
+        setTimeout(function() {
+          ripple.remove();
+        }, 800);
+  
+      });
+    };
+  }(jQuery));
   
   (function($){
   $(document).ready(function(){
@@ -76,5 +114,9 @@
      title: "Menu",
      format: "multitoggle"
   });
+
+  $('.ripple-effect').rkmd_rippleEffect();
+
   });
+
   })(jQuery);
